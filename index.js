68002ca1b170d826955a31c9347679a3df72bfae
mdsahinsiraj <mdsahinsiraj2@gmail.com>
2025-07-05 18:59:30 +0600

    add server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log("Hi")
-console.log("Hello")
-console.log("Wrld")
+
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+    console.log(req)
+})
+server.listen(5000, ()=>{
+    console.log('Server is listening')
+})
